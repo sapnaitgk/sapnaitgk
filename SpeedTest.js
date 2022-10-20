eb3257abdd86d8c9dbbad5310d451a5bd8f56caf
sapnaitgk <95950405+sapnaitgk@users.noreply.github.com>
2022-10-20 22:41:05 +0530

    Update SpeedTest.js

--- SpeedTest.js
+++ SpeedTest.js
@@ -1,1 +1,7 @@
-Hello
+var speedtest =`<div style="text-align:right;">
+  <div style="min-height:360px;">
+    <div style="width:100%;height:0;padding-bottom:50%;position:relative;">
+      <iframe style="border:none;position:absolute;top:0;left:0;width:100%;height:100%;min-height:360px;border:none;overflow:hidden !important;" src="//openspeedtest.com/Get-widget.php"></iframe>
+    </div>
+  </div>Provided by <a href="https://sapnaitgk.com" rel="dofollow" target="_blank">sapnaitgk.com</a></div>`
+document.write(speedtest);
